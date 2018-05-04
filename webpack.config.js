5e20ff8b5e1ee5e3c0bcb321fefa547db5d66bc4
kevhoque <kevhoque@publicisgroupe.net>
2018-05-04 18:18:27 +0200

    start integration template

--- webpack.config.js
+++ webpack.config.js
@@ -11,14 +11,20 @@ Encore
     // .enableVersioning(Encore.isProduction())
 
     // uncomment to define the assets of the project
+    .addEntry('js/animsition', './assets/vendor/animsition/js/animsition.js')
+    .addEntry('js/main', './assets/js/main.js')
     .addEntry('js/app', './assets/js/app.js')
     .addStyleEntry('css/app', './assets/css/app.scss')
+
+    .createSharedEntry('vendor', [
+        'jquery',
+    ])
 
     // uncomment if you use Sass/SCSS files
     .enableSassLoader()
 
     // uncomment for legacy applications that require $/jQuery as a global variable
-    // .autoProvidejQuery()
+    .autoProvidejQuery()
 ;
 
 module.exports = Encore.getWebpackConfig();
